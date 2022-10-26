7d0ec3261cfaca0973f1b6ac1b6e6cf9692b7d94
Dennis Mureti <dennismureti47@gmail.com>
2022-10-26 17:07:57 +0300

    node dependencies install and API dev

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 console.log('Hello, World');
+const express = require('express')
+// import 'package:express/express.dart'
+const PORT = 3000;
+
+const app = express();
+
+// CREATING NODE API
+app.get('/hello-world', (req, res) => {
+    res.send('hello world')
+});
+// GET, PUT, POST DELETE, UPDATE 
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+});
