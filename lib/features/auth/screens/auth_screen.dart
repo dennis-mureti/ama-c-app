b0b28e58d611ef5da9e282ab6f9f4a58c40f0fc4
Dennis Mureti <dennismureti47@gmail.com>
2022-10-26 14:58:39 +0300

    create sign up and sign in form

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone_tutorial/common/widgets/custom_textfield.dart';
+import 'package:amazon_clone_tutorial/common/widgets/custome_button.dart';
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
@@ -42,12 +43,16 @@ class _AuthScreenState extends State<AuthScreen> {
         padding: const EdgeInsets.all(8.0),
         // welcome widget
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
               'Welcome',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
             ),
             ListTile(
+              tileColor: _auth == Auth.signup
+                  ? GlobalVariables.backgroundColor
+                  : GlobalVariables.greyBackgroundCOlor,
               title: const Text('Create Account',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -64,19 +69,40 @@ class _AuthScreenState extends State<AuthScreen> {
               ),
             ),
             if (_auth == Auth.signup)
-              Form(
-                key: _signUpFormKey,
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      controller: _emailController,
-                      hintText: 'Email',
-                    )
-                  ],
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: GlobalVariables.backgroundColor,
+                child: Form(
+                  key: _signUpFormKey,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _nameController,
+                        hintText: 'Name',
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hintText: 'Email',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomButton(text: 'Sign Up', onTap: () {})
+                    ],
+                  ),
                 ),
               ),
             // sign in radio
             ListTile(
+              tileColor: _auth == Auth.signin
+                  ? GlobalVariables.backgroundColor
+                  : GlobalVariables.greyBackgroundCOlor,
               title: const Text('Sign In',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -91,7 +117,33 @@ class _AuthScreenState extends State<AuthScreen> {
                   });
                 },
               ),
-            )
+            ),
+            if (_auth == Auth.signin)
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: GlobalVariables.backgroundColor,
+                child: Form(
+                  key: _signUpFormKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hintText: 'Email',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomButton(text: 'Sign In ', onTap: () {})
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       )),
