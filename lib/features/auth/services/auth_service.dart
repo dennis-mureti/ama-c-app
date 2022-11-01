0cf963b9695e5881741d5874066dbb504f722007
Dennis Mureti <dennismureti47@gmail.com>
2022-11-01 14:12:43 +0300

    User model for authentication

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,21 @@
+import 'package:amazon_clone_tutorial/models/user.dart';
+// import 'package:http/http.dart' as http;
+
+class AuthService {
+  // sign up user
+  void signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      User user = User(
+          id: '',
+          name: name,
+          password: password,
+          address: '',
+          type: '',
+          token: ''); 
+    } catch (e) {}
+  }
+}
