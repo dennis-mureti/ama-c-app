c5926c516cbd800f63ce5cd2e2c7f110e55e680e
Dennis Mureti <dennismureti47@gmail.com>
2022-11-17 10:39:30 +0300

    account page, add widgets->account button, below app bar, orders, top button

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -1,4 +1,7 @@
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
+import 'package:amazon_clone_tutorial/features/account/widgets/below_app_bar.dart';
+import 'package:amazon_clone_tutorial/features/account/widgets/orders.dart';
+import 'package:amazon_clone_tutorial/features/account/widgets/top_buttons.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -27,9 +30,32 @@ class AccountScreen extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
+              Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        right: 15,
+                      ),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(Icons.search),
+                  ],
+                ),
+              )
             ],
           ),
         ),
+      ),
+      body: Column(
+        children: const [
+          BelowAppBar(),
+          SizedBox(height: 10),
+          TopButtons(),
+          SizedBox(height: 20),
+          Orders()
+        ],
       ),
     );
   }
