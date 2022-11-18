7aa72ce6fb4a8419444dd32d4811c79c6b0d786c
Dennis Mureti <dennismureti47@gmail.com>
2022-11-18 15:00:35 +0300

    create carousel widget and add to homescreen

---
+++ lib/features/home/widgets/deal_of_day.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DealOfDay extends StatefulWidget {
+  const DealOfDay({Key? key}) : super(key: key);
+
+  @override
+  State<DealOfDay> createState() => _DealOfDayState();
+}
+
+class _DealOfDayState extends State<DealOfDay> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(left: 10, top: 15),
+          child: const Text(
+            'Deal of the day',
+            style: TextStyle(fontSize: 20  ),
+          ),
+        )
+      ],
+    );
+  }
+}
