9246f578b26e07765e08fd90a4bae872857810bb
Dennis Mureti <dennismureti47@gmail.com>
2022-11-22 17:56:46 +0300

    start create admin page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone_tutorial/common/widgets/bottom_bar.dart';
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
+import 'package:amazon_clone_tutorial/features/admin/screens/admin_screen.dart';
 import 'package:amazon_clone_tutorial/features/auth/services/auth_service.dart';
 import 'package:amazon_clone_tutorial/providers/user_provider.dart';
 import 'package:amazon_clone_tutorial/router.dart';
@@ -58,7 +59,9 @@ class _MyAppState extends State<MyApp> {
               ))),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? const BottomBar()
+          ? Provider.of<UserProvider>(context).user.type == 'user'
+              ? const BottomBar()
+              : const AdminScreen()
           : const AuthScreen(),
     );
   }
