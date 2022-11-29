0696ec8d6ea3fb7dfe085f9989695e1d058e34dc
Dennis Mureti <dennismureti47@gmail.com>
2022-11-29 10:02:49 +0300

    create serverside (product)middleware, models, routes

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:amazon_clone_tutorial/constants/utils.dart';
+import 'package:amazon_clone_tutorial/models/product.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
 
@@ -20,10 +21,18 @@ class AdmiServices {
 
       for (int i = 0; i < images.length; i++) {
         CloudinaryResponse res = await cloudinary.uploadFile(
-          CloudinaryFile.fromFile(images[i].path),
+          CloudinaryFile.fromFile(images[i].path, folder: name),
         );
         imageUrls.add(res.secureUrl);
       }
+
+      Product product = Product(
+          name: name,
+          description: description,
+          quantity: quantity,
+          images: imageUrls,
+          category: category,
+          price: price);
     } catch (e) {
       showSnackBar(context, e.toString());
     }
