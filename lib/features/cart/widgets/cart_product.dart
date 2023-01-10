a57bd1ebe1c1880eedecb26ced82702f0b043f74
Dennis Mureti <dennismureti47@gmail.com>
2023-01-10 10:09:03 +0300

    increased and decreased quantity

--- lib/features/cart/widgets/cart_product.dart
+++ lib/features/cart/widgets/cart_product.dart
@@ -1,3 +1,5 @@
+import 'package:amazon_clone_tutorial/features/cart/services/cart_services.dart';
+import 'package:amazon_clone_tutorial/features/product_details/services/product_details_services.dart';
 import 'package:amazon_clone_tutorial/models/product.dart';
 import 'package:amazon_clone_tutorial/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +17,24 @@ class CartProduct extends StatefulWidget {
 }
 
 class _CartProductState extends State<CartProduct> {
+  final ProductDetailsServices productDetailsServices =
+      ProductDetailsServices();
+  final CartServices cartServices = CartServices();
+
+  void increaseQuantity(Product product) {
+    productDetailsServices.addToCart(
+      context: context,
+      product: product,
+    );
+  }
+
+  void decreaseQuantity(Product product) {
+    cartServices.removeFromCart (
+      context: context,
+      product: product,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final productCart = context.watch<UserProvider>().user.cart[widget.index];
@@ -23,58 +43,61 @@ class _CartProductState extends State<CartProduct> {
 
     return Column(
       children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
-            children: [
-              Image.network(
-                product.images[0],
-                fit: BoxFit.contain,
-                height: 135,
-                width: 135,
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: 235,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      product.name,
-                      style: const TextStyle(
-                        fontSize: 16,
+        InkWell(
+          onTap: () => decreaseQuantity(product),
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                Image.network(
+                  product.images[0],
+                  fit: BoxFit.contain,
+                  height: 135,
+                  width: 135,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 235,
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                        maxLines: 2,
                       ),
-                      maxLines: 2,
                     ),
-                  ),
-                  Container(
-                    width: 235,
-                    padding: const EdgeInsets.only(left: 10, top: 5),
-                    child: Text(
-                      '\$${product.price}',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                    Container(
+                      width: 235,
+                      padding: const EdgeInsets.only(left: 10, top: 5),
+                      child: Text(
+                        '\$${product.price}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
                       ),
-                      maxLines: 2,
                     ),
-                  ),
-                  Container(
-                    width: 235,
-                    padding: const EdgeInsets.only(left: 10),
-                    child: const Text('Eligible for FREE Shipping'),
-                  ),
-                  Container(
-                    width: 235,
-                    padding: const EdgeInsets.only(left: 10, top: 5),
-                    child: const Text(
-                      'In Stock',
-                      style: TextStyle(color: Colors.teal),
-                      maxLines: 2,
+                    Container(
+                      width: 235,
+                      padding: const EdgeInsets.only(left: 10),
+                      child: const Text('Eligible for FREE Shipping'),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    Container(
+                      width: 235,
+                      padding: const EdgeInsets.only(left: 10, top: 5),
+                      child: const Text(
+                        'In Stock',
+                        style: TextStyle(color: Colors.teal),
+                        maxLines: 2,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
         Container(
@@ -104,9 +127,10 @@ class _CartProductState extends State<CartProduct> {
                     ),
                     DecoratedBox(
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black12, width: 1.5),
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(0)),
+                        border: Border.all(color: Colors.black12, width: 1.5),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(0),
+                      ),
                       child: Container(
                         width: 35,
                         height: 32,
@@ -116,13 +140,16 @@ class _CartProductState extends State<CartProduct> {
                         ),
                       ),
                     ),
-                     Container(
-                      width: 35,
-                      height: 32,
-                      alignment: Alignment.center,
-                      child: const Icon(
-                        Icons.add,
-                        size: 18,
+                    InkWell(
+                      onTap: () => increaseQuantity(product),
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.add,
+                          size: 18,
+                        ),
                       ),
                     ),
                   ],
