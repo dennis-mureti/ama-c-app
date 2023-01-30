42b2035b71b27d6dd9b37cb8bf395496ecc8f2cd
Dennis Mureti <dennismureti47@gmail.com>
2023-01-30 16:05:01 +0300

    order details page view prooducts

--- lib/features/order_details/screens/order_details.dart
+++ lib/features/order_details/screens/order_details.dart
@@ -130,7 +130,54 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
                     Text('Order ID: \$${widget.order.totalPrice}')
                   ],
                 ),
-              )
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Purchase Details',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                // width: double.infinity,
+                // padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    for (int i = 0; i < widget.order.products.length; i++)
+                      Row(
+                        children: [
+                          Image.network(
+                            widget.order.products[i].images[0],
+                            height: 120,
+                            width: 120,
+                          ),
+                          const SizedBox(width: 5),
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.order.products[i].name,
+                                style: const TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Qty: ${widget.order.quantity[i]}',
+                              )
+                            ],
+                          ))
+                        ],
+                      )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
